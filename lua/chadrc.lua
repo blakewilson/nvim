c5c0c06c5fc47aff6278375a6b9aff964612405a
Blake Wilson <blake@blake.id>
2024-05-07 13:35:58 -0500

    Sync changes

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "tokyodark",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,31 +7,54 @@ return {
     end,
   },
 
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+ {
+   "neovim/nvim-lspconfig",
+   config = function()
+     require("nvchad.configs.lspconfig").defaults()
+     require "configs.lspconfig"
+   end,
+ },
 
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+ {
+ 	"williamboman/mason.nvim",
+ 	opts = {
+ 		ensure_installed = {
+ 			"lua-language-server",
+      "stylua",
+ 			"html-lsp",
+      "css-lsp" ,
+      "prettier",
+      "typescript-language-server",
+      "svelte-language-server",
+      "tailwindcss-language-server",
+      "markdownlint",
+      "json-lsp",
+      "html-lsp",
+      "eslint-lsp",
+      "codespell",
+      "bash-language-server",
+      "graphql-language-service-cli"
+ 		},
+ 	},
+ },
+
+
+ -- require('telescope').setup{ 
+ --   defaults = {
+ --     file_ignore_patterns = {
+ --       "node_modules",
+ --       "dist"
+ --     }
+ --   }
+ -- }
+
+ -- {
+ -- 	"nvim-treesitter/nvim-treesitter",
+ -- 	opts = {
+ -- 		ensure_installed = {
+ -- 			"vim", "lua", "vimdoc",
+ --      "html", "css"
+ -- 		},
+ -- 	},
+ -- },
 }
